028f83491dbdb95abb7143609ab856632f148974
Dallas-Alfonso <51836155+Dallas-Alfonso@users.noreply.github.com>
2020-06-10 15:47:25 -0400

    cProj

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
+const Chart = ({ data: { confirmed, recovered , deaths }, country}) => {
+    const [dailyData, setDailyData] = useState({}); // [] reaplced by {}
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const initialDailyData = await fetchDailyData(); //setDailyData(await fetchDailyData()); replaced const initialDailyData = await fetchDailyData();
+        
+            setDailyData(initialDailyData); //added
+        }
+
+        fetchAPI();
+    }, []);
+
+    const barChart = (
+        confirmed
+        ? (
+          <Bar 
+          data={{
+              labels: ['Infected', 'Recovered', 'Deaths'],
+              datasets: [{
+                  label: 'People',
+                  backgroundColor: [
+                      'rgb(255,140,0)',
+                      'rgb(155, 229, 170)',
+                      'rgba(255, 0,0,0.5)'
+                  ],
+                  data:[confirmed.value, recovered.value, deaths.value],
+              },
+            ],
+          }}
+          options={{
+              legend: {display: false},
+              title: {display: true, text: `Current state in ${country}`},
+          }}
+          />  
+        ) : null
+    );
+
+    //.length replaced by [0]  Changes within <Line confirmed replaced by data, deaths reaplced by data
+    const lineChart = (
+        dailyData[0] 
+        ? (
+        <Line 
+        data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [{
+                data: dailyData.map(( data ) => data.confirmed),
+                label: 'Infected',
+                borderColor: '#FF7F50',
+                fill: true,
+            }, {
+                data: dailyData.map(( data ) => data.deaths),
+                label: 'Deaths',
+                borderColor: 'red',
+                /*backgroundColor: 'rgba(255,0,0,0.5)',*/
+                fill: true,
+            }
+            ], 
+            }}
+        />
+        ) :null
+    );
+
+    return (
+        <div className={styles.container}>
+            {country ? barChart : lineChart}
+        </div>
+    );
+};
+
+export default Chart;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
+
+import styles from './CountryPicker.module.css';
+
+//CountryPicker (was replaced by Countries)
+// Line 33:16. Error fix HERES
+const Countries = ({ handleCountryChange }) => {
+    const [countries, setCountries] = useState([]);
+
+//fetchedCountries, setFetchedCountries (replaced by countries, setCountries)
+
+
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setCountries (await fetchCountries());
+        };
+        //setFetchedCountries replaced by setCountries
+
+        fetchAPI();
+    }, []);
+//setFetchedCountries replaced by BLANK 
+    
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+    {countries.map((country, i) => <option key={i} value={country}>{country}</option> )}
+            </NativeSelect>
+        </FormControl>
+    );
+};
+// fetchedCountries replaced by countries
+
+export default Countries;
+
+//CountryPicker replaced by Countries
